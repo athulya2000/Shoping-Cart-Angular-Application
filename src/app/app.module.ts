5b08a498934cdb97b25577ea2996e27faed1ffb2
athulya2000 <athulyav00@gmail.com>
2022-11-25 14:48:14 +0530

    commited

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,23 +5,29 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AddProductComponent } from './add-product/add-product.component';
 import { RouterModule, Routes } from '@angular/router';
+import { ViewAllproductsComponent } from './view-allproducts/view-allproducts.component';
 
 const myRoute:Routes=[
   {
     path:"",
     component:AddProductComponent
+  },
+  {
+    path:"view",
+    component:ViewAllproductsComponent
   }
 ]
 
 @NgModule({
   declarations: [
     AppComponent,
-    AddProductComponent
+    AddProductComponent,
+    ViewAllproductsComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    RouterModule
+    RouterModule.forRoot(myRoute)
   ],
   providers: [],
   bootstrap: [AppComponent]
